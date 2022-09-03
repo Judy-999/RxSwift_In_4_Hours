37024437cfb3066d1aed0267bb492a167012625b
Judy <joo00ee@naver.com>
2022-09-03 18:40:41 +0900

    sugar api - just, from

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -46,47 +46,16 @@ class ViewController: UIViewController {
 
     // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
     func downloadJson(_ url: String) -> Observable<String?> {
-        return Observable.create { emiiter in
-            let url = URL(string: MEMBER_LIST_URL)!
-            
-            let task = URLSession.shared.dataTask(with: url) { data, _, error in
-                guard error == nil else {
-                    emiiter.onError(error!) // 에러가 발생하면 에러를 전송
-                    return
-                }
-                
-                if let data = data, let json = String(data: data, encoding: .utf8) {
-                    emiiter.onNext(json)    // 데이터가 준비되면 데이터를 전송
-                }
-                
-                emiiter.onCompleted()   // 데이터가 준비되지 않았어도 종료
-            }
-                
-            task.resume()
-            
-            return Disposables.create() {
-                task.cancel()   // downloadJson를 subscribe 했을 때 dispose 하면 이 부분을 실행
-            }
-        }
+        // 어차피 데이터 하나만 보낼거고 에러도 안날거야
+        return Observable.just("Hello") // sugar api
         
+        // 여러개 보내고 싶으면? element마다 전달
+        return Observable.from(["Hello", "World"])
+        
+//        위의 한줄이 아래 다섯줄과 같음
 //        return Observable.create { emitter in
 //            emitter.onNext("Hello")
-//            emitter.onNext("World") // 여러 개를 보낼 수 있음
 //            emitter.onCompleted()
-//
-//            return Disposables.create()
-//        }
-        
-//        return Observable.create { f in
-//            DispatchQueue.global().async {
-//                let url = URL(string: MEMBER_LIST_URL)!
-//                let data = try! Data(contentsOf: url)
-//                let json = String(data: data, encoding: .utf8)
-//                DispatchQueue.main.async {
-//                    f.onNext(json)
-//                    f.onCompleted()
-//                }
-//            }
 //
 //            return Disposables.create()
 //        }
@@ -102,12 +71,11 @@ class ViewController: UIViewController {
         
         // 2. Observable로 오는 데이터를 받아서 처리하는 방법
         _ = downloadJson(MEMBER_LIST_URL)
-            .debug()    // 어떤 이벤트가 전달되는지 찍힘
             .subscribe { event in
                 switch event {
                 case .next(let json):
                     DispatchQueue.main.async {
-                        self.editView.text = json   // URLSession을 처리하는 스레드에서 넘어왔기 때문에 main 스레드가 아님
+                        self.editView.text = json
                         self.setVisibleWithAnimation(self.activityIndicator, false)
                     }
                 case .completed:
@@ -119,6 +87,4 @@ class ViewController: UIViewController {
     }
 }
 
-// Rxswift의 사용법 알기
-// 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
-// 2. Observable로 오는 데이터를 받아서 처리하는 방법
+// 이 기본 사용법 마저도 길고 귀찮다! -> Sugar
