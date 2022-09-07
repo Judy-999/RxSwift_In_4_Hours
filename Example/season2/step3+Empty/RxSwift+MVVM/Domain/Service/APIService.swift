1c4cae9d3f1fa5bfb0e6dd5a2acdb669b62242eb
Judy <joo00ee@naver.com>
2022-09-07 10:42:00 +0900

    API에서 Session으로 받아오는 함수를 rx 버전으로 추가

--- Example/season2/step3+Empty/RxSwift+MVVM/Domain/Service/APIService.swift
+++ Example/season2/step3+Empty/RxSwift+MVVM/Domain/Service/APIService.swift
@@ -7,8 +7,9 @@
 //
 
 import Foundation
+import RxSwift
 
-let MenuUrl = "https://firebasestorage.googleapis.com/v0/b/rxswiftin4hours.appspot.com/o/fried_menus.json?alt=media&token=42d5cb7e-8ec4-48f9-bf39-3049e796c936"
+let MenuUrl = "https://firebasestorage.googleapis.com/v0/b/rxswiftin4hours.appspot.com/o/fried_menus.json?alt=media&token=42d5cb7e-8ec4-48f9-bf39-3049e796c936" // 서버에서 제공하는 API
 
 class APIService {
     static func fetchAllMenus(onComplete: @escaping (Result<Data, Error>) -> Void) {
@@ -26,5 +27,22 @@ class APIService {
             }
             onComplete(.success(data))
         }.resume()
+    }
+    
+    // API에서 제이슨을 받아오면 name과 price 밖애 없음 -> model 타입과 달리서 fetch 해온걸 컨버터해줘야 함
+    // rx로 바꿀 때 fetchAllMenus 메서드를 이용할 수 있음
+    static func fetchAllMenusRx() -> Observable<Data> { // Data만 반환, 어차피 에러는 onError로 나옴
+        Observable.create { emitter in
+            fetchAllMenus { result in
+                switch result {
+                case .success(let data):
+                    emitter.onNext(data)
+                    emitter.onCompleted()
+                case .failure(let error):
+                    emitter.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
     }
 }
